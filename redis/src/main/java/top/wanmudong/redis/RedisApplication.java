1ad36a94aee8031a3957cf74e4059c7f647b9ec5
wanmudong <1903172774@qq.com>
2018-12-11 13:03:49 +0800

    整合redis

---
+++ redis/src/main/java/top/wanmudong/redis/RedisApplication.java
@@ -0,0 +1,16 @@
+package top.wanmudong.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import redis.clients.jedis.JedisPoolConfig;
+
+@SpringBootApplication
+public class RedisApplication {
+
+    public static void main(String[] args) {
+
+        JedisPoolConfig config=new JedisPoolConfig();
+
+        SpringApplication.run(RedisApplication.class, args);
+    }
+}
