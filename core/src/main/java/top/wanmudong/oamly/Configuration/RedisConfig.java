f5b58397651c04f45b2f8a100d90d671bbc9a8ec
wanmudong <1903172774@qq.com>
2019-04-13 13:34:37 +0800

    通过 alexxiyang/shiro-redis 插件实现了redis缓存session

---
+++ core/src/main/java/top/wanmudong/oamly/Configuration/RedisConfig.java
@@ -0,0 +1,30 @@
+package top.wanmudong.oamly.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+public class RedisConfig {
+
+
+    private String host= "6379";
+
+    private int timeout = 2*60*60*1000;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
+}
